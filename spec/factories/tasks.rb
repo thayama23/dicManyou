852713efd0acffa01a2827d3738588cc52f04345
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-03-01 18:30:15 +0900

    dicManyou_step1 ER図修正 push4

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :task do
-    task_name { 'test_title' }
-    task_detail { 'test_content' }
+    name { 'test_title' }
+    detail { 'test_content' }
   end
+
+  factory :second_task, class: Task do
+   name { 'Factoryで作ったデフォルトのタイトル２' }
+   detail { 'Factoryで作ったデフォルトのコンテント２' }
+ end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,15 +1,28 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    # 「タスク一覧画面」や「タスク詳細画面」などそれぞれのテストケースで、before内のコードが実行される
+    # 各テストで使用するタスクを1件作成する
+    # 作成したタスクオブジェクトを各テストケースで呼び出せるようにインスタンス変数に代入
+    @task = FactoryBot.create(:task, title: 'task')
+  end
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
-      it '作成済みのタスクが表示されること' do
-        task = FactoryBot.create(:task, task_name: 'task')
-
+      it '作成済みのタスクが表示されること'
+        # beforeに必要なタスクデータが作成されるので、ここでテストデータ作成処理を書く必要がない
         visit tasks_path
         expect(page).to have_content 'task'
       end
     end
-  end
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日時の降順に並んでいること' do
+        new_task = FactoryBot.create(:task, title: 'new_task')
+        visit tasks_path
+        task_list = all('.task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
+        expect(task_list[0]).to have_content 'new_task'
+        expect(task_list[1]).to have_content 'task'
+      end
+    end
 
   describe 'タスク登録画面' do
     context '必要項目を入力して、createボタンを押した場合' do
