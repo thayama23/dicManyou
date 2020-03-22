e1349fb386f48f218ce035ff029cd6883eccaa6c
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-03-22 11:34:01 +0900

    dicManyou_stpe4 飯田さんが加えて下さった場所を一部削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include SessionsHelper
+
+
+    # def forbid_login_user
+    #     redirect_to tasks_path, notice: 'すでにログインしています' if current_user
+    # end
+
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def forbid_login_user
+        redirect_to tasks_path, notice: 'すでにログインしています' if current_user
+      end
 end
